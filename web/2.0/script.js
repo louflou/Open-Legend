283baa3a6b76f6423ecbec5ed0653a90c861564c
Lou <louise@flou.eu>
2017-04-18 19:27:27 +0200

    christian hjälp

--- web/2.0/script.js
+++ web/2.0/script.js
@@ -1,55 +1,51 @@
 
 $(document).ready(function(){
-    var score = $(".attr-table .attr-score");
-    score.change(function(){
-        $(this).parent().next().children('.attr-cost').val('500');   
-        
-    });
-    
+    var scoreField = $(".attr-score");
+    var score = scoreField.val();
+    scoreField.change(function(){
+        scoreCostDice(score);
+        $(this).parent().next().children('.attr-cost').val(cost);           
+    });    
 });
 
-
-/* nedan är vad vi vill göra men fungerar inte.
-
-
-function scoreCostDice(){
-    var score = $(this).val();
+function scoreCostDice(score){
     var cost = calcCost(score);
     var dice = calcDice(score);
-    $(this).parent().next().children(.attr-cost).val(cost);
-    $(this).parent().next().children(.attr-dice).val(dice);
+    alert('cost = ' + cost
+         + ' dice = ' + dice);
+    $(this).parent().next().children('.attr-cost').val(cost);
+    $(this).parent().next().children('.attr-dice').val(dice);
 }
 
 function calcCost(score){
-    if score == 0 {
-        return = 0;
-    }else if score == 1 {
-        return = 1;
-    }else if score == 2{
-        return = 3;
-    }else if score == 3 {
-        return = 6;
-    }else if score == 4 {
-        return = 10;
-    }else if score == 5 {
-        return = 15;
+    if(score == 0) {
+        return 0;
+    }else if(score == 1) {
+        return 1;
+    }else if(score == 2) {
+        return 3;
+    }else if(score == 3) {
+        return 6;
+    }else if(score == 4) {
+        return 10;
+    }else if(score == 5) {
+        return 15;
     }
 }
 
 function calcDice(score){
-    if score == 0 {
-        return = "1d4";
-    }else if score == 1 {
-        return = "1d4";
-    }else if score == 2{
-        return = "1d6";
-    }else if score == 3 {
-        return = "1d8";
-    }else if score == 4 {
-        return = "1d10";
-    }else if score == 5 {
-        return = "2d6";
+    if(score == 0) {
+        return "1d4";
+    }else if(score == 1) {
+        return "1d4";
+    }else if(score == 2) {
+        return "1d6";
+    }else if(score == 3) {
+        return "1d8";
+    }else if(score == 4) {
+        return "1d10";
+    }else if(score == 5) {
+        return "2d6";
     }
 }
 
-*/
