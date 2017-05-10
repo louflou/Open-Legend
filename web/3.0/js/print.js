36c4583f3eac43e638e4ba28710afd0f82e3d7a4
Lou <louise@flou.eu>
2017-05-10 12:53:00 +0200

    print id osv

--- web/3.0/js/print.js
+++ web/3.0/js/print.js
@@ -50,6 +50,36 @@ $(document).ready(function(){
        cost: $('#cost-deception').val(),
        dice: $('#dice-deception').val()
    }
+   var presence = {
+       score: $('#input-presence').val(),
+       cost: $('#cost-presence').val(),
+       dice: $('#dice-presence').val()
+   }
+   var persuasion = {
+       score: $('#input-persuasion').val(),
+       cost: $('#cost-persuasion').val(),
+       dice: $('#dice-persuasion').val()
+   }
+   var learning = {
+       score: $('#input-learning').val(),
+       cost: $('#cost-learning').val(),
+       dice: $('#dice-learning').val()
+   }
+   var logic = {
+       score: $('#input-logic').val(),
+       cost: $('#cost-logic').val(),
+       dice: $('#dice-logic').val()
+   }
+   var perception = {
+       score: $('#input-perception').val(),
+       cost: $('#cost-perception').val(),
+       dice: $('#dice-perception').val()
+   }
+   var will = {
+       score: $('#input-will').val(),
+       cost: $('#cost-will').val(),
+       dice: $('#dice-will').val()
+   }
    
    
    
