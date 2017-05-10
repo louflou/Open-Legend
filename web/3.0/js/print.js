97a172f69fd4e6a92a61395ff979a07c627c9cb7
Lou <louise@flou.eu>
2017-05-10 12:31:50 +0200

    commit för sync

--- web/3.0/js/print.js
+++ web/3.0/js/print.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-   var characterInfo = {
+  // Character Info 
+    var stats = {
        level: $('#levelValue').val(),
        xp: $('#xp-input').val(),
        wealth: $('#wealth-input').val(),
        speed: $('#speed-input').val()
    }
-   alert(' XP: ' + characterInfo.xp);
+   var info = {
+       cName: $('#c-name').val(),
+       pName: $('#p-name').val(),
+       archetype: $('#archetype-select').val(),
+       description: $('#description-text').val()
+   }
+   var perksFlaws = {
+       perks: $('#perks-select').val(),
+       flaws: $('#flaws-select').val()  
+   }
+   var featPoints = {
+       total: $('#feat-total-points').val(),
+       invested: $('#feat-invested-points').val(),
+       available: $('#feat-available-points').val()
+   }
+   
+   $('#perks-select').on('select', alert(perksFlaws.perks));
+   
+   
 });
